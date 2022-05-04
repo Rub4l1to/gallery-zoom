37e841f59bc753c2067adb33cca64b901883311f
Adrian Perez <adrian.perez.garcia2018@gmail.com>
2022-05-05 00:04:59 +0200

    Functionality with sass stylish

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import './style.css';
+import './styles/style.scss';
+
+import reactImg from './assets/react-img.png';
+import vueImg from './assets/vue-img.jpeg';
+import angularImg from './assets/angular-img.webp';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <div class="container">
+    <div class="panel "
+      style="background-image: url(${reactImg})">
+      <h3 class="panel__title">React</h3>
+    </div>
+    <div class="panel "
+      style="background-image:url(${vueImg})">
+      <h3 class="panel__title">Vue</h3>
+    </div>
+    <div class="panel "
+      style="background-image:url(${angularImg})">
+      <h3 class="panel__title">Angular </h3>
+    </div>
+  </div>
 `;
+
+const panels = document.querySelectorAll<HTMLDivElement>('.panel');
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
+
+window.onload = () => {
+  panels.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        removeActiveClasses();
+        item.classList.add('active');
+      }
+    });
+  });
+};
